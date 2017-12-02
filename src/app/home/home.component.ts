b8e6db38fb8068cb2fcdd62fcabd1d78c197bfcb
sarathks <sarathsivrud@gmail.com>
2017-12-02 20:54:36 +0530

    changed styles and scripts for responsive design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,11 +16,11 @@ export class HomeComponent implements OnInit {
 			$('body').css("position","fixed"); // For setting the background static
 
 			//START--following code is to set width of photostack in home page for responsiveness
-			$('.stack').append('<style>.stack{width:'+(window.innerWidth-30)+'px !important;}</style>');
-			$('.stack').append('<style>.stack:before{width:'+(window.innerWidth-50)+'px !important;}</style>');
-			$('.stack').append('<style>.stack:after{width:'+(window.innerWidth-30)+'px !important;}</style>');
+			$('.stack').append('<style>.stack{width:'+(window.innerWidth-65)+'px !important;}</style>');
+			$('.stack').append('<style>.stack:before{width:'+(window.innerWidth-65)+'px !important;}</style>');
+			$('.stack').append('<style>.stack:after{width:'+(window.innerWidth-65)+'px !important;}</style>');
 			var img = $("#photostack")[0];
-			img.width = window.innerWidth-30;
+			img.width = window.innerWidth-65;
 			//END..........
 		}
 	}
