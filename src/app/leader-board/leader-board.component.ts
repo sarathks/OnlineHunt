46d35bb4149df70c6c5c54e093e3a9ca7aec0a18
sarathks <sarathsivrud@gmail.com>
2018-01-24 23:39:51 +0530

    quick cahnges

--- src/app/leader-board/leader-board.component.ts
+++ src/app/leader-board/leader-board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Router } from '@angular/router';
+declare var $:any;
 
 
 @Component({
@@ -10,11 +11,61 @@ import { Router } from '@angular/router';
 })
 export class LeaderBoardComponent implements OnInit {
 
+
+  public loaderInLeaderBoard:any = false;
+  public modalTitle:any;
+  public rankList:any;
+
+
   constructor(private router:Router, private DataService:DataService) { }
 
   ngOnInit() {
   if(localStorage.access_token) {
+    const pointer = this;
+    pointer.loaderInLeaderBoard = true;
+    pointer.DataService.fetchUserDetails("/users/leader-board").
+    subscribe(
+      (data) => {
+        pointer.loaderInLeaderBoard = false;
+        if(data.json().code ==0 )
+        {
+          pointer.rankList = data.json().Payload;
+        }
+        else
+        {
+          if(data.json().message)
+          {
+            pointer.modalTitle = "Error";
+            localStorage.message = data.json().message;
+            $("#operationSuccess").modal("show");      
+          }
+          else
+          {
+            pointer.modalTitle = "Error";
+            localStorage.message = "Some error occured";
+            $("#operationSuccess").modal("show");
+          }
+        } 
+    },
+    function(err){
+      pointer.loaderInLeaderBoard = false;
+      if(err.json().message)
+      {
+        
+        localStorage.message = err.json().message;
+        $("#operationSuccess").modal("show");      
+      }
+      else {
+        pointer.modalTitle = "Error";
+        localStorage.message = "Internet failure Or Server error occured";
+        $("#operationSuccess").modal("show");
+      }
+      
+    }
+    );
+
   }
+
   else {
   this.router.navigate([''])
   }
